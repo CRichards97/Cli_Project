465003e1371b764d5e76c9c7fb2024ef0f645ba3
CRichards97 <Richardsclint17@gmail.com>
2021-09-19 13:55:11 -0500

    initial commit

---
+++ Cli_Project/omaha_autolots.rb
@@ -0,0 +1,8 @@
+require 'pry'
+require 'httparty'
+require 'dotenv/load'
+
+require_relative "Cli_Project/autolots.rb"
+require_relative "Cli_Project/autolot_api.rb"
+require_relative "Cli_Project/autolot_cli.rb"
+require_relative "Cli_Project/version.rb"
